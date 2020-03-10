efb3ea75685f43fad47e13c74f909bdbc3f4b767
jstrombe19 <jaredstromberg@gmail.com>
2020-03-10 16:37:27 -0600

    Added 'create' action in movies controller as well as the routes.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,4 +6,17 @@ class MoviesController < ApplicationController
     render json: @movies 
   end
 
+  def create
+    @movie = Movie.create(
+      title: params[:title],
+      rating: params[:rating],
+      genre: params[:genre],
+      release_date: params[:release_date],
+      runtime: params[:runtime],
+      synopsis: params[:synopsis]
+    )
+
+    render json: @movie 
+  end
+
 end
